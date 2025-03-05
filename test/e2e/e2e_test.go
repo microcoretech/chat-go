c57e48047eb327f3a8355091becf4a98472a6b43
Horiodino <holiodin@gmail.com>
2025-03-06 00:22:50 +0530

    added e2e for /chats endpoint
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -15,6 +15,7 @@
 package e2e
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -26,6 +27,10 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 
+	chat "chat-go/internal/chat/http"
+	"chat-go/internal/common/domain"
+	user "chat-go/internal/common/http"
+
 	"chat-go/internal/common/common"
 	"chat-go/internal/infrastructure/api"
 	"chat-go/test/util"
@@ -33,17 +38,55 @@ import (
 
 var _ = ginkgo.Describe("Chat", func() {
 	var client *http.Client
+	var adminToken string
+	var adminUserId uint64
 
 	ginkgo.BeforeEach(func() {
 		client = &http.Client{
 			Timeout: 5 * time.Second,
 		}
+
+		ginkgo.DeferCleanup(func() {
+			client.CloseIdleConnections()
+		})
+
+		signInRequest := map[string]any{
+			"password": "mohnIeih4Ju9zHYE1VPWL0mHyzBjyFPl",
+			"username": "admin",
+		}
+
+		reqBody, err := json.Marshal(signInRequest)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		signInReq, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/auth/sign-in", chatURL), bytes.NewBuffer(reqBody))
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		signInReq.Header.Set("Content-Type", "application/json")
+
+		signInResp, err := client.Do(signInReq)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		body, err := io.ReadAll(signInResp.Body)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		defer signInResp.Body.Close()
+
+		gomega.Expect(signInResp.StatusCode).To(gomega.Equal(http.StatusOK))
+
+		var tokenResponse map[string]interface{}
+		gomega.Expect(json.Unmarshal(body, &tokenResponse)).To(gomega.Succeed())
+		adminToken = tokenResponse["token"].(string)
+
+		user, ok := tokenResponse["user"].(map[string]interface{})
+		if !ok {
+			ginkgo.Fail("Sign-in response missing 'user' field")
+		}
+		adminUserId = uint64(user["id"].(float64))
 	})
 
 	ginkgo.Context("root endpoint", func() {
 		ginkgo.It("should return valid response", func() {
 			resp, err := client.Get(chatURL)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			defer resp.Body.Close()
 			gomega.Expect(resp.StatusCode).To(gomega.Equal(http.StatusOK))
 
 			body, err := io.ReadAll(resp.Body)
@@ -67,11 +110,89 @@ var _ = ginkgo.Describe("Chat", func() {
 		ginkgo.It("should return valid response", func() {
 			resp, err := client.Get(fmt.Sprintf("%s/healthz", chatURL))
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			defer resp.Body.Close()
 			gomega.Expect(resp.StatusCode).To(gomega.Equal(http.StatusOK))
 
 			body, err := io.ReadAll(resp.Body)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 			gomega.Expect(string(body)).To(gomega.Equal("OK"))
 		})
+	})
+
+	//  TODO ADD CLEANUP TEST
+
+	ginkgo.It("should return valid response for create group chat", func() {
+		createChatRequest := chat.CreateChatDto{
+			Name: "Test Group Chat",
+			Type: 2,
+		}
+		chatReqBody, err := json.Marshal(createChatRequest)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/chats", chatURL), bytes.NewBuffer(chatReqBody))
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", adminToken))
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := client.Do(req)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		body, err := io.ReadAll(resp.Body)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		defer resp.Body.Close()
+
+		gomega.Expect(resp.StatusCode).To(gomega.Equal(http.StatusOK))
+
+		var chatResponse chat.ChatDto
+		gomega.Expect(json.Unmarshal(body, &chatResponse)).To(gomega.Succeed())
+
+		expectedChatResponse := chat.ChatDto{
+			ID:          chatResponse.ID,
+			Name:        "Test Group Chat",
+			Type:        2,
+			Image:       domain.Image{},
+			LastMessage: nil,
+			CreatedBy:   adminUserId,
+			Creator: &user.UserDto{
+				ID:        adminUserId,
+				Email:     "admin@gmail.com",
+				Username:  "admin",
+				Role:      2,
+				FirstName: "Admin",
+				LastName:  "Admin",
+				AboutMe:   "",
+				Image:     domain.Image{},
+				CreatedAt: chatResponse.Creator.CreatedAt,
+				UpdatedAt: chatResponse.Creator.UpdatedAt,
+			},
+			UserChats: []chat.UserChatDto{
+				{
+					UserID: adminUserId,
+					ChatID: chatResponse.ID,
+					User: &user.UserDto{
+						ID:        adminUserId,
+						Email:     "admin@gmail.com",
+						Username:  "admin",
+						Role:      2,
+						FirstName: "Admin",
+						LastName:  "Admin",
+						AboutMe:   "",
+						Image:     domain.Image{},
+						CreatedAt: chatResponse.UserChats[0].User.CreatedAt,
+						UpdatedAt: chatResponse.UserChats[0].User.UpdatedAt,
+					},
+				},
+			},
+			CreatedAt: chatResponse.CreatedAt,
+			UpdatedAt: chatResponse.UpdatedAt,
+		}
+
+		gomega.Expect(chatResponse).To(gomega.BeComparableTo(expectedChatResponse))
+		gomega.Expect(chatResponse.CreatedAt).ToNot(gomega.BeZero())
+		gomega.Expect(chatResponse.UpdatedAt).ToNot(gomega.BeZero())
+		gomega.Expect(chatResponse.Creator.CreatedAt).ToNot(gomega.BeZero())
+		gomega.Expect(chatResponse.Creator.UpdatedAt).ToNot(gomega.BeZero())
+		gomega.Expect(chatResponse.UserChats[0].User.CreatedAt).ToNot(gomega.BeZero())
+		gomega.Expect(chatResponse.UserChats[0].User.UpdatedAt).ToNot(gomega.BeZero())
 	})
 })
